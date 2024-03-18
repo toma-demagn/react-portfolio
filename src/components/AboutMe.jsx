d2ed0a5fbdfb2f319ac54efe13984ec2e2223963
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-03-19 00:55:51 +0100

    Tweaked everything

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,55 +1,65 @@
 import styled from "styled-components";
-import { useSelector } from "react-redux";
-import { selectData } from "../pages/homeSlice";
-import { Element } from "react-scroll";
+import {useSelector} from "react-redux";
+import {selectData} from "../pages/homeSlice";
+import {Element} from "react-scroll";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 // Data
-import { moreInfo } from "../data";
+import {moreInfo} from "../data";
 // Components
-import { Col, Container, Row } from "react-bootstrap";
-import { Title } from "./globalStyledComponents";
+import {Col, Container, Row} from "react-bootstrap";
+import {Title} from "./globalStyledComponents";
+import cnesLogo from '../images/cnes_logo.png';
+
+
 
 const StyledAboutMe = styled.section`
-  p {
-    font-size: 1.25rem;
-  }
-  .img {
-    width: 18rem;
-    height: 18rem;
-  }
+    p {
+        font-size: 1.25rem;
+    }
+
+    .img {
+        width: 18rem;
+        height: 18rem;
+    }
 `;
 
 export default function AboutMe() {
-  const { avatar_url, bio } = useSelector(selectData);
+    const {avatar_url, bio} = useSelector(selectData);
 
-  return (
-    <Element name={"About"} id="about">
-      <StyledAboutMe className="section">
-        <Container>
-          <Container className="d-flex">
-            <Title>
-              <h2>About Me</h2>
-              <div className="underline"></div>
-            </Title>
-          </Container>
-          <Row className="align-items-center mt-5">
-            <Col className="d-flex flex-column text-center">
-              <Container>
-                <p>{bio}</p>
-                {moreInfo && <p>{moreInfo}</p>}
-              </Container>
-            </Col>
-            <Col className="d-none d-md-block text-center">
-              <img
-                src={avatar_url}
-                alt="GitHub Avatar"
-                loading="lazy"
-                className="mx-auto rounded-circle"
-                style={{ width: "15rem", height: "15rem" }}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </StyledAboutMe>
-    </Element>
-  );
+    return (
+        <Element name={"About"} id="about">
+            <StyledAboutMe className="section">
+                <Container>
+                    <Container className="d-flex">
+                        <Title>
+                            <h2>About Me</h2>
+                            <div className="underline"></div>
+                        </Title>
+                    </Container>
+                    <Row className="align-items-center mt-5">
+                        <Col className="d-flex flex-column text-center">
+                            <Container>
+                                <p>{bio}</p>
+                                {moreInfo && <p>{moreInfo}</p>}
+                            </Container>
+                        </Col>
+                        <Col className="d-none d-md-block text-center">
+                            <img
+                                src={avatar_url}
+                                alt="GitHub Avatar"
+                                loading="lazy"
+                                className="mx-auto rounded-circle"
+                                style={{width: "15rem", height: "15rem"}}
+                            />
+                        </Col>
+                    </Row>
+                    <Container className="mt-xxl-5 text-center">
+                        <h3>Current position: Space surveillance at <img src={cnesLogo} alt="CNES Logo" style={{height: '1.3em'}}/></h3>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Toulouse, France</p>
+                    </Container>
+                </Container>
+            </StyledAboutMe>
+        </Element>
+    );
 }
--- src/data.js
+++ src/data.js
@@ -32,7 +32,9 @@ export const Blog = <Icon icon="ph:link-bold" />;
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, React Bootstrap, Redux, and the GitHub REST API.";
+  "I'm a Software and Data engineer. I enjoy learning about new technologies that " +
+    "could benefit my creating abilities. In parallel, I am also a cyclist and calisthenics practionner, I have worked on " +
+    "personal projects I can use to improve in these fields. Take a look at my projects below !";
 
 /* Skills
  ************************************************************** 
@@ -41,48 +43,48 @@ export const moreInfo =
 export const skillData = [
   {
     id: 1,
-    skill: <Icon icon="mdi:language-html5" className="display-4" />,
-    name: "HTML5",
+    skill: <Icon icon="logos:python" className="display-4" />,
+    name: "Python",
   },
   {
     id: 2,
-    skill: <Icon icon="ion:logo-css3" className="display-4" />,
-    name: "CSS3",
+    skill: <Icon icon="carbon:sql" className="display-4" />,
+    name: "SQL",
   },
   {
     id: 3,
-    skill: <Icon icon="fa6-brands:js" className="display-4" />,
-    name: "JavaScript",
+    skill: <Icon icon="cib:mongodb" className="display-4" />,
+    name: "MongoDB",
   },
   {
     id: 4,
-    skill: <Icon icon="ri:bootstrap-fill" className="display-4" />,
-    name: "BootStrap",
+    skill: <Icon icon="cib:docker-icon" className="display-4" />,
+    name: "Docker",
   },
   {
     id: 5,
-    skill: <Icon icon="mdi:react" className="display-4" />,
-    name: "React",
+    skill: <Icon icon="fa-brands:github" className="display-4" />,
+    name: "GitHub",
   },
   {
     id: 6,
-    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
-    name: "Styled Components",
+    skill: <Icon icon="cib:gitlab" className="display-4" />,
+    name: "GitLab CI/CD",
   },
   {
     id: 7,
-    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
-    name: "Redux",
+    skill: <Icon icon="logos:java" className="display-4" />,
+    name: "Java",
   },
   {
     id: 8,
-    skill: <Icon icon="bi:git" className="display-4" />,
-    name: "Git",
+    skill: <Icon icon="logos:react" className="display-4" />,
+    name: "React",
   },
   {
     id: 9,
-    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
-    name: "GitHub",
+    skill: <Icon icon="logos:nodejs-icon" className="display-4" />,
+    name: "Node.js",
   },
 ];
 
